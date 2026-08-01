45bbc978f1f51dfbfe3eaca292eddc88680e73bd
arjindersingh <arjindersingh@hotmail.com>
2026-08-01 06:04:53 +0000

    Welcome to Home

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import * as bootstrap from 'bootstrap';
+
+// Make Bootstrap's programmatic API available to inline scripts and other
+// project code (for example: new bootstrap.Modal('#exampleModal')).
+window.bootstrap = bootstrap;
